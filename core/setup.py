50684c5fbb20e1777bdd3948fa3e61ace6d0ba3a
Amirouche Boubekki <amirouche.boubekki@gmail.com>
2013-11-16 14:19:18 +0100

    setup.py: import version from pythonium_core

--- core/setup.py
+++ core/setup.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 from setuptools import setup
 
+from pythonium_core import __version__
+
 
 setup(
     name='pythonium-core',
-    version='0.1',
+    version=__version__,
     description='Python 3 to Javascript translator written in Python that produce fast portable javascript code',
     author='Amirouche Boubekki',
     author_email='amirouche.boubekki@gmail.com',
