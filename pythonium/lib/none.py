914a61782d93e5500a9a96584aada087e3a02546
Amirouche Boubekki <amirouche.boubekki@gmail.com>
2013-11-30 19:16:53 +0100

    added None

---
+++ pythonium/lib/none.py
@@ -0,0 +1,16 @@
+class _None:
+
+    def __and__(self, other):
+        if other is True:
+            return True
+        return False
+
+    def __or__(self, other):
+        if other is True:
+            return True
+        return False        
+
+    def __is__(self, other):
+        if JS('other === self'):
+            return True
+        return False
