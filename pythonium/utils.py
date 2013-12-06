ac0f6978e012c71cbd43e30407ee9215edd2d7bd
Amirouche Boubekki <amirouche.boubekki@gmail.com>
2013-12-06 23:40:41 +0100

    fix nested generator definition
    if a generator was defined in a function both function were dubbed as generator functions

--- pythonium/utils.py
+++ pythonium/utils.py
@@ -5,3 +5,7 @@ class YieldSearch(NodeVisitor):
 
     def visit_Yield(self, node):
         self.has_yield = True
+
+    def visit_FunctionDef(self, node):
+        pass  # do not visit nest function definition to determine
+              # if a function has a yield or not...
