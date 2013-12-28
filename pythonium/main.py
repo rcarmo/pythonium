707f75d145f00130c5ef54b34f531a899a4fb633
amz3 <amz3@hypermove.net>
2013-12-28 23:02:54 +0100

    bump 0.6.2

--- pythonium/main.py
+++ pythonium/main.py
@@ -25,7 +25,7 @@ from .veloce.veloce import Veloce
 from .compliant.compliant import Compliant
 from .utils import pythonium_generate_js
 
-__version__ = '0.6.1'
+__version__ = '0.6.2'
 
 
 def main(argv=None):
