f7e465dfffe867ac468570d12a30cde7a118947a
skariel <skariel@gmail.com>
2014-01-01 20:26:05 +0200

    fix test

--- tests/lambda.py
+++ tests/lambda.py
@@ -1,5 +1,4 @@
 def func(x, y, arg, kwargs):
-    return x + y + arg[0] + arg[1]
-    print(kwargs["key"])
+    return x + y + arg[0] + arg[1] + kwargs["key"]
 f = lambda x, y, *args, **kwargs: func(x, y, args, kwargs) + 1
 print f(42, 1337, 11, 100, key="value")
