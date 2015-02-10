599759586f8f5c58a369ca3cfda288835434aa86
Jason Crockett <deddokatana@gmail.com>
2015-02-10 22:51:43 +0000

    switched from using django-bower to setuptools_bower

--- pythonium/BowerLoad/JsLibraryAnalyser.py
+++ pythonium/BowerLoad/JsLibraryAnalyser.py
@@ -1,13 +1,13 @@
 __author__ = 'Jason'
 
-import glob
+import re
 import os
 import fnmatch
 
 
 class Mapper:
-    #map out files{} -> classes{} -> functions{{inputs:[],returns:[]}
-    #moduleMap = files{classes{functions{{inputs:[],returns:[]}}
+    #map out files{} -> classes{} & Imports{} -> functions{{inputs:[],returns:[]}
+    #moduleMap = files{{classes{functions{{inputs:[],returns:[]},"imports":[]}
     moduleMap = {"files":{}}
     RootJsfileList = []
     RootDir = os.curdir() #or js library folder path
@@ -17,7 +17,18 @@ class Mapper:
         for root, dirnames, filenames in os.walk(RootDir):
             for filename in fnmatch.filter(filenames, '*.js'):
                 self.moduleMap["files"] += {str(filename):""}
+    def find_imports_in_file(self):
+        imports = re.findall()
+        return imports
+    def find_all_classes_in_file(self):
         pass
+    def find_all_functions_in_class(self):
+        pass
+    def get_inputs_from_function(self):
+        pass
+    def parseJSfile(self):
+        pass
+
 
 class Skelmaker:
     #Create Skeleton Python Modules For Easy Ide Intergration
--- pythonium/BowerLoad/loader.py
+++ pythonium/BowerLoad/loader.py
@@ -1,18 +1,19 @@
 __author__ = 'Jason'
 
-from pythonium.BowerLoad import bower,JsLibraryAnalyser
+from pythonium.BowerLoad import JsLibraryAnalyser
+import setuptools_bower
 
 class BowerLoader:
     def __init__(self):
         if not bower.BowerAdapter.is_bower_exists():
             print("please install bower from the system terminal/command prompt with 'npm install bower")
             raise Exception("Bower Is Not Installed or Accessable from the system console Path")
+        bower = setuptools_bower()
 
     def createLibrarySkeletons(self,PackageName):
         LibraryMap = JsLibraryAnalyser
         return LibraryMap
 
     def load(self,PackageName):
-        bower.install(PackageName)
         self.createLibrarySkeletons(PackageName)
         pass
