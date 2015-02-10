e384aee40e41d36c2ff32b76a1d4d162e0c9cecc
Jason Crockett <deddokatana@gmail.com>
2015-02-10 00:09:32 +0000

    Stopping for the night. corrected JsLibraryAnalyser.js

--- pythonium/BowerLoad/JsLibraryAnalyser.py
+++ pythonium/BowerLoad/JsLibraryAnalyser.py
@@ -2,18 +2,21 @@ __author__ = 'Jason'
 
 import glob
 import os
+import fnmatch
 
 
 class Mapper:
-    #map out files -> classes -> functions+returns
-    moduleMap = {}
+    #map out files{} -> classes{} -> functions{{inputs:[],returns:[]}
+    #moduleMap = files{classes{functions{{inputs:[],returns:[]}}
+    moduleMap = {"files":{}}
     RootJsfileList = []
+    RootDir = os.curdir() #or js library folder path
     def __init__(self):
         pass
-    def _find_entry_Points(self):
-        os.chdir("/mydir")
-        for file in glob.glob("*.js"):
-            print(file)
+    def find_all_js_files(self,RootDir=RootDir):
+        for root, dirnames, filenames in os.walk(RootDir):
+            for filename in fnmatch.filter(filenames, '*.js'):
+                self.moduleMap["files"] += {str(filename):""}
         pass
 
 class Skelmaker:
